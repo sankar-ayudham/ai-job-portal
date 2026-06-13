c397ad9592257911d92f976094d695e3733633b3
sankar58 <ayudhamsankar@gmail.com>
2026-06-13 22:27:26 +0530

    Fixed CORS origin and Axios baseURL

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 app.use(cors({
     origin: [
         'http://localhost:5173', 
-        'https://vercel.com/sankar58s-projects/ai-job-portal' 
+        'https://ai-job-portal-sandy.vercel.app/' 
     ],
     credentials: true
 }));
